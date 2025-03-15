a2a17a1d2550f9ba14e1e6f63d8408eb11091e93
Mohankumaram1 <132732401+Mohankumaram1@users.noreply.github.com>
2025-03-15 08:15:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "aws_instance" "worker_ansible" {
   ami             = "ami-00bb6a80f01f03502"  # Ubuntu 22.04 LTS (Mumbai)
   instance_type   = "t2.medium"
   subnet_id       = data.aws_subnet.default.id
-  security_group_ids = [aws_security_group.k8s_sg.id]  # Fixed Issue
+  vpc_security_group_ids = [aws_security_group.k8s_sg.id]  # Fixed Issue
   key_name        = "mohanm"
 
   tags = {
